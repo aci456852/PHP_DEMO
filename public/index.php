b53cb1372761379df8e8d7408ef92492d2a2bcac
aci456852 <1084665808@qq.com>
2019-05-20 15:36:10 +0800

    在index.php增加路径权限限制

--- public/index.php
+++ public/index.php
@@ -2,14 +2,36 @@
 
 $route=isset($_GET['r'])?$_GET['r']:NULL;
 set_include_path(get_include_path().PATH_SEPARATOR.'../');
-session_start();
 if($route)
 {
+	session_start();
 	$partials=explode("/", $route);
 	if(count($partials)!=2)
 	{
 		die('invalid route');
 	}
+	if($partials[0]!='login')
+	{
+		if(!isset($_SESSION['user']))
+		{
+			header('Location:/index.php?r=login/login_page');
+		}
+		$user=$_SESSION['user'];
+		if($user['role']=='student'){
+			if($partials[0]!='student'){
+				die('权限不足');
+			}
+		}
+		else if($user['role']=='teacher'){
+			if(!in_array($partials[0], ['teacher','assignment'])){
+				die('权限不足');
+			}
+		}
+		else{
+			die('权限不足');
+		}
+	}
+
 	$filename=$partials[0];
 	$class_name=ucfirst(strtolower($filename))."Controller";
 	$function_name=$partials[1];
